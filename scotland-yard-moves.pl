36352511fd7d84778fba3cfcbb2f66e7a2c11d7f
Ignavum <timsener276@gmail.com>
2022-04-19 16:24:26 +0200

    1-done

--- scotland-yard-moves.pl
+++ scotland-yard-moves.pl
@@ -9,3 +9,7 @@ scotlandYardMoveBi(From,To,Using) :-
 
 %! scotlandYardMove(?From:int, ?To:int, ?Using:atom) is nondet
 scotlandYardMove(1,8,cab).
+scotlandYardMove(1,9,cab).
+scotlandYardMove(1,46,bus).
+scotlandYardMove(1,46,underground).
+scotlandYardMove(1,58,bus).
