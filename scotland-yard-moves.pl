273682842f4b61855b42b1fbd2d167cbe1c957fa
Ignavum <timsener276@gmail.com>
2022-04-20 21:06:03 +0200

    added explicit comparason

--- scotland-yard-moves.pl
+++ scotland-yard-moves.pl
@@ -1,10 +1,14 @@
 :- module(scotlandyardmoves,[scotlandYardMoveBi/3, scotlandYardMove/3]).
 
+:- use_module(library(clpfd)).
+
 %! scotlandYardMoveBi(?From:int, ?To:int, ?Using:atom) is nondet
 scotlandYardMoveBi(From,To,Using) :-
+    From #< To,
     scotlandYardMove(From,To,Using).
 %! scotlandYardMoveBi(?From:int, ?To:int, ?Using:atom) is nondet
 scotlandYardMoveBi(From,To,Using) :-
+    From #> To,
     scotlandYardMove(To,From,Using).
 
 % all scotlandYardMove must be of the form scotlandYardMove(X,Y,Z) where X < Y. to achive bidiractional relations see scotlandYardMoveBi
